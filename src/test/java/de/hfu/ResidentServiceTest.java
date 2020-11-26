49a19ac0ca745a3dfedf10cb0b3740b9efa845cb
Dominik Merkt <dominik.merkt@hs-furtwangen.de>
2020-11-26 14:27:02 +0100

    Änderungen an den Residents vorgenommen

--- src/test/java/de/hfu/ResidentServiceTest.java
+++ src/test/java/de/hfu/ResidentServiceTest.java
@@ -16,9 +16,9 @@ public class ResidentServiceTest {
 	
 	@Test
 	public void getUniqueResidentTest() {
-		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Weigheim" , new Date(1983-05-17) );
-		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Weingarten" , new Date(1999-02-27) );
-		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Berlin" , new Date(1956-11-05) );
+		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Durchhausen" , new Date(1983-05-17) );
+		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Durchhausen" , new Date(1999-02-27) );
+		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Durchhausen" , new Date(1956-11-05) );
 		
 		ResidentRepositoryStub testRepository = new ResidentRepositoryStub();
 		
@@ -40,9 +40,9 @@ public class ResidentServiceTest {
 	
 	@Test(expected = ResidentServiceException.class)
 	public void getUniqueResidentExceptionNotUniqueTest() throws ResidentServiceException {
-		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Weigheim" , new Date(1983-05-17) );
-		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Weingarten" , new Date(1999-02-27) );
-		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Berlin" , new Date(1956-11-05) );
+		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Durchhausen" , new Date(1983-05-17) );
+		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Durchhausen" , new Date(1999-02-27) );
+		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Durchhausen" , new Date(1956-11-05) );
 		
 		ResidentRepositoryStub testRepository = new ResidentRepositoryStub();
 		
@@ -61,9 +61,9 @@ public class ResidentServiceTest {
 	
 	@Test(expected = ResidentServiceException.class)
 	public void getUniqueResidentExceptionWildcardTest() throws ResidentServiceException {
-		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Weigheim" , new Date(1983-05-17) );
-		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Weingarten" , new Date(1999-02-27) );
-		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Berlin" , new Date(1956-11-05) );
+		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Durchhausen" , new Date(1983-05-17) );
+		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Durchhausen" , new Date(1999-02-27) );
+		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Durchhausen" , new Date(1956-11-05) );
 		
 		ResidentRepositoryStub testRepository = new ResidentRepositoryStub();
 		
@@ -81,10 +81,10 @@ public class ResidentServiceTest {
 	}
 	
 	@Test
-	public void getFilteredResidentsListForWildcardTest() {
-		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Weigheim" , new Date(1983-05-17) );
-		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Weingarten" , new Date(1999-02-27) );
-		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Berlin" , new Date(1956-11-05) );
+	public void getFilteredResidentsListByWildcardTest() {
+		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Durchhausen" , new Date(1983-05-17) );
+		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Durchhausen" , new Date(1999-02-27) );
+		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Durchhausen" , new Date(1956-11-05) );
 		
 		ResidentRepositoryStub testRepository = new ResidentRepositoryStub();
 		
@@ -124,10 +124,9 @@ public class ResidentServiceTest {
 	
 	@Test
 	public void getFilteredResidentsListByCity() {
-		
-		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Weigheim" , new Date(1983-05-17) );
-		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Weingarten" , new Date(1999-02-27) );
-		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Berlin" , new Date(1956-11-05) );
+		Resident resident1 = new Resident("Josef", "Maier", "Kronstraße 1", "Durchhausen" , new Date(1983-05-17) );
+		Resident resident2 = new Resident("Günter", "Papenheimer", "Weinstraße 231", "Durchhausen" , new Date(1999-02-27) );
+		Resident resident3 = new Resident("Jaqueline", "Guldi", "Siegesstraße 43", "Durchhausen" , new Date(1956-11-05) );
 		
 		ResidentRepositoryStub testRepository = new ResidentRepositoryStub();
 		
